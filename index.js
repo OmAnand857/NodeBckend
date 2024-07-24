00e8682e324c01702234ea4ec0431fea920705cc
OmAnand857 <allansmith2561@gmail.com>
2024-07-24 14:05:03 +0530

    .weektmotpriod

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ const app = express();
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
-let memoryArray = [];
+let memoryArray = []; // can have const db=db.connect(){..blah..blah} using array for now
 
 app.post('/form',(req,res)=>{
     const formData = req.body;
+    console.log(formData);
     memoryArray.push(formData);
     res.render('success.ejs',{
         name : formData.name,
         email: formData.email,
         phoneNumber:formData.phoneNumber,
-        password:formData.password
+        password:formData.password,
+        period : formData.checkbox
     });
-
 })
 app.get('/',(req,res)=>{
         res.render('index.ejs');
